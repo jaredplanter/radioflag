02c7300d6a8c0de2fe86888ba1e43ce285842935
Jared Planter <jaredplanter@gmail.com>
2016-08-05 13:41:37 -0700

    Configure will_paginate to work with the MusixMatch search_artist response
    Use WillPaginate::Collection to add pagination to an array

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,15 @@
 class SearchController < ApplicationController
+  require 'will_paginate/array'
+
   def index
     redirect_to action: "results" if artist_name.present?
   end
 
   def results
     set_artist_name_cookie
-    @results = MusixMatch.search_artist({q_artist: artist_name}).artist_list
+    @results = WillPaginate::Collection.create(current_page, per_page, search_response.available) do |pager|
+        pager.replace search_response.artist_list
+    end
   end
 
   private
@@ -18,5 +22,17 @@ class SearchController < ApplicationController
     if params.has_key? :artist_name
       cookies[:artist_name] = params[:artist_name]
     end
+  end
+
+  def current_page
+    params[:page] || 1
+  end
+
+  def per_page
+    10
+  end
+
+  def search_response
+    MusixMatch.search_artist({q_artist: artist_name, page: current_page, page_size: per_page})
   end
 end
