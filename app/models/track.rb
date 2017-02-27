27c3b4a83fdaeffe4a5efd85b7d8b4497647ffea
Jared Planter <jaredplanter@gmail.com>
2017-02-27 07:07:47 -0800

    Wrap MusixMatch track object variables

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,9 +1,23 @@
-class Track
+class Track < Struct.new(:track)
   def self.select_by_album_id album_id
-    new.select_by_album_id album_id
+    MusixMatch.get_album_tracks(album_id).track_list.map do |track|
+      new track
+    end
   end
 
-  def select_by_album_id album_id
-    MusixMatch.get_album_tracks(album_id).track_list
+  def name
+    track.track_name
+  end
+
+  def album_name
+    track.album_name
+  end
+
+  def length
+    track.track_length
+  end
+
+  def cover_art
+    track.album_coverart_100x100
   end
 end
