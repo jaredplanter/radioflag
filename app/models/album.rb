eb9ec674e5f5e80aa1d2a4cb5b67aac114c9d6e2
Jared Planter <jaredplanter@gmail.com>
2017-02-27 07:00:40 -0800

    Wrap MusixMatch album object variables

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,9 +1,15 @@
-class Album
+class Album < Struct.new(:album)
   def self.select_by_artist_id artist_id
-    new.select_by_artist_id artist_id
+    MusixMatch.get_artist_albums(artist_id).album_list.map do |album|
+      new album
+    end
   end
 
-  def select_by_artist_id artist_id
-    MusixMatch.get_artist_albums(artist_id).album_list
+  def id
+    album.album_id
+  end
+
+  def cover_art
+    album.album_coverart_100x100
   end
 end
