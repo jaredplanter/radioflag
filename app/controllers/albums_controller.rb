914bf587f873dc83f1e5fbd57c2d58fa9856810b
Jared Planter <jaredplanter@gmail.com>
2017-02-26 21:03:42 -0800

    Remove album_name instance variable from AlbumsController

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,7 +1,6 @@
 class AlbumsController < ApplicationController
   def show
     @tracks ||= MusixMatch.get_album_tracks(params[:id]).track_list
-    @album_name = @tracks.first.album_name
     render layout: false
   end
 end
